826bba84fafb42306d5faeac32fe12ae3c440b57
Aleksandr Orlenko <aorlenko@yelp.com>
2019-03-29 14:05:20 +0000

    zipkin support

--- http_echo/main.go
+++ http_echo/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
+
+	zipkin "github.com/openzipkin/zipkin-go"
+	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
+	zipkinhttpreporter "github.com/openzipkin/zipkin-go/reporter/http"
 )
 
-var capability string
+var (
+	serviceName = os.Getenv("SERVICE_NAME")
+	capability  = os.Getenv("SERVICE_CAPABILITY")
+
+	zipkinHTTPClient *zipkinhttp.Client
+)
 
 type ResponseData struct {
 	Proto          string
@@ -18,10 +28,38 @@ type ResponseData struct {
 	RequestHeaders http.Header
 	RequestBody    string `json:",omitempty"`
 	Capability     string
+	ServiceName    string
+	SubreqData     string `json:",omitempty"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	if serviceName == "echo_subreq" {
+		w.WriteHeader(200)
+		w.Write([]byte("hello from subrequest"))
+		return
+	}
+
 	body, _ := ioutil.ReadAll(r.Body)
+
+	// Make a sub request
+	subreqData := func() string {
+		req, err := http.NewRequest("GET", "http://srv_echo_subreq:8080/hello", nil)
+		if err != nil {
+			log.Printf("unable to create http request: %+v\n", err)
+			return ""
+		}
+
+		res, err := zipkinHTTPClient.DoWithAppSpan(req, "hello_function")
+		if err != nil {
+			log.Printf("unable to do http request: %+v\n", err)
+			return ""
+		}
+		defer res.Body.Close()
+
+		subreqBody, _ := ioutil.ReadAll(res.Body)
+		return string(subreqBody)
+	}()
+	// -----------------
 
 	respData := &ResponseData{
 		Proto:          r.Proto,
@@ -29,6 +67,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		RequestHeaders: r.Header,
 		RequestBody:    string(body),
 		Capability:     capability,
+		ServiceName:    serviceName,
+		SubreqData:     subreqData,
 	}
 
 	w.WriteHeader(200)
@@ -38,9 +78,31 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	capability = os.Getenv("SERVICE_CAPABILITY")
+	// Zipkin support
+	reporter := zipkinhttpreporter.NewReporter("http://zipkin:9411/api/v2/spans")
+	defer reporter.Close()
+
+	zipkinEndpoint, err := zipkin.NewEndpoint(serviceName, "")
+	if err != nil {
+		log.Fatalf("unable to create local endpoint: %+v\n", err)
+	}
+
+	zipkinTracer, err := zipkin.NewTracer(reporter, zipkin.WithLocalEndpoint(zipkinEndpoint))
+	if err != nil {
+		log.Fatalf("unable to create tracer: %+v\n", err)
+	}
+
+	zipkinHTTPClient, err = zipkinhttp.NewClient(zipkinTracer, zipkinhttp.ClientTrace(true))
+	if err != nil {
+		log.Fatalf("unable to create client: %+v\n", err)
+	}
+
+	zipkinMiddleware := zipkinhttp.NewServerMiddleware(zipkinTracer, zipkinhttp.TagResponseSize(true))
+	// --------------
 
 	h2s := &http2.Server{}
 
-	http.ListenAndServe(":8080", h2c.NewHandler(http.HandlerFunc(handler), h2s))
+	http.ListenAndServe(":8080", h2c.NewHandler(
+		zipkinMiddleware(http.HandlerFunc(handler)),
+		h2s))
 }
